f330f40b26c76911d1abb547fe2a467e80c13bf2
Will Bryant <william@tout.com>
2015-06-11 13:53:16 -0700

    don't use concern

--- lib/slack_chatter/api/namespaces.rb
+++ lib/slack_chatter/api/namespaces.rb
@@ -16,65 +16,63 @@ require_relative './users.rb'
 module SlackChatter
   module Api
     module Namespaces
-      extend ActiveSupport::Concern
 
-      included do
-        def api
-          @api ||= SlackChatter::Api::Api.new(self)
-        end
-
-        def auth
-          @auth ||= SlackChatter::Api::Auth.new(self)
-        end
-
-        def channels
-          @channels ||= SlackChatter::Api::Channels.new(self)
-        end
-
-        def chat
-          @chat ||= SlackChatter::Api::Chat.new(self)
-        end
-
-        def emoji
-          @emoji ||= SlackChatter::Api::Emoji.new(self)
-        end
-
-        def files
-          @files ||= SlackChatter::Api::Files.new(self)
-        end
-
-        def groups
-          @groups ||= SlackChatter::Api::Groups.new(self)
-        end
-
-        def im
-          @im ||= SlackChatter::Api::Im.new(self)
-        end
-
-        def oauth
-          @oath ||= SlackChatter::Api::Oauth.new(self)
-        end
-
-        def rtm
-          @rtm ||= SlackChatter::Api::Rtm.new(self)
-        end
-
-        def search
-          @search ||= SlackChatter::Api::Search.new(self)
-        end
-
-        def stars
-          @stars ||= SlackChatter::Api::Stars.new(self)
-        end
-
-        def team
-          @team ||= SlackChatter::Api::Team.new(self)
-        end
-
-        def users
-          @users ||= SlackChatter::Api::Users.new(self)
-        end
+      def api
+        @api ||= SlackChatter::Api::Api.new(self)
       end
+
+      def auth
+        @auth ||= SlackChatter::Api::Auth.new(self)
+      end
+
+      def channels
+        @channels ||= SlackChatter::Api::Channels.new(self)
+      end
+
+      def chat
+        @chat ||= SlackChatter::Api::Chat.new(self)
+      end
+
+      def emoji
+        @emoji ||= SlackChatter::Api::Emoji.new(self)
+      end
+
+      def files
+        @files ||= SlackChatter::Api::Files.new(self)
+      end
+
+      def groups
+        @groups ||= SlackChatter::Api::Groups.new(self)
+      end
+
+      def im
+        @im ||= SlackChatter::Api::Im.new(self)
+      end
+
+      def oauth
+        @oath ||= SlackChatter::Api::Oauth.new(self)
+      end
+
+      def rtm
+        @rtm ||= SlackChatter::Api::Rtm.new(self)
+      end
+
+      def search
+        @search ||= SlackChatter::Api::Search.new(self)
+      end
+
+      def stars
+        @stars ||= SlackChatter::Api::Stars.new(self)
+      end
+
+      def team
+        @team ||= SlackChatter::Api::Team.new(self)
+      end
+
+      def users
+        @users ||= SlackChatter::Api::Users.new(self)
+      end
+
 
     end
   end
